3d55d2a0346f8dded4c4074b18fa1b3f067cdd13
Evadne Wu <ev@radi.ws>
2017-06-01 00:20:53 +0100

    adds working solution but exit trapping required

--- lib/sample/listener.ex
+++ lib/sample/listener.ex
@@ -8,19 +8,34 @@ defmodule Sample.Listener do
   end
   
   def init(_) do
-    try do
-      {:ok, Postgrex.Notifications.start_link([
-        otp_app: :sample,
-        adapter: Ecto.Adapters.Postgres,
-        username: "hello",
-        password: "world",
-        database: "bogus",
-        hostname: "localhost",
-        port: 16384
-      ])}
-    rescue
-      DBConnection.ConnectionError ->
-        {:ok, :error}
+    Process.flag(:trap_exit, true)
+    Process.send_after(self(), :attempt_connect, 500)
+    {:ok, :pending}
+  end
+  
+  def handle_info(:attempt_connect, state) do
+    config = [
+      otp_app: :sample,
+      adapter: Ecto.Adapters.Postgres,
+      username: "hello",
+      password: "world",
+      database: "bogus",
+      hostname: "localhost",
+      port: 16384,
+      sync_connect: true
+    ]
+    case Postgrex.Notifications.start_link(config) do
+      {:ok, pid} -> {:noreply, pid}
+      {:error, reason} ->
+        IO.inspect [:attempt_connect, reason]
+        Process.send_after(self(), :attempt_connect, 500)
+        {:noreply, :error}
     end
+  end
+
+  def handle_info(_, :error), do: {:noreply, :error} 
+  def handle_info({:EXIT, pid, reason}, pid) do
+    Process.send_after(self(), :attempt_connect, 500)
+    {:noreply, :error}
   end
 end
